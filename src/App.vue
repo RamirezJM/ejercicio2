d35e81a0384b2324991cc1b9894b41b4badd5709
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-02-03 22:42:37 -0300

    feat: agregar archivos de proyecto, ejecutar deploy

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from "vue"
+import Tarjeta from "./components/Tarjeta.vue";
+
+/* En lugar del objeto data de la Options API, uso la
+forma similar en la Composition API, ref() */
+
+const mascotas = ref([
+  {
+    id: 1,
+    nombre: "Firulais",
+    tipo: "Perro",
+    edad: 4,
+    vacunado: true,
+    foto: "https://placedog.net/500/350?id=8",
+    caracteristicas: ["Juguetón", "Cariñoso", "Sociable"]
+  },
+  {
+    id: 2,
+    nombre: "Michi",
+    tipo: "Gato",
+    edad: 2,
+    vacunado: false,
+    foto: "https://placecats.com/500/350",
+    caracteristicas: ["Curioso", "Dormilón", "Independiente"]
+  },
+  {
+    id: 3,
+    nombre: "Rocky",
+    tipo: "Perro",
+    edad: 7,
+    vacunado: true,
+    foto: "https://placedog.net/500/350?id=5",
+    caracteristicas: ["Tranquilo", "Leal"]
+  }
+])
+</script>
+
+<template>
+
+  <h1>Elige tu mascota ideal</h1>
+  <section class="mascotas">
+    <Tarjeta v-for="({ id, nombre, tipo, edad, vacunado, foto, caracteristicas }) in mascotas" :key="id" 
+    :nombre="nombre"
+    :tipo="tipo" 
+    :edad="edad" 
+    :vacunado="vacunado" 
+    :foto="foto" 
+    :caracteristicas="caracteristicas" />
+  </section>
+</template>
+
+<style>
+h1{
+  text-align: center;
+  margin: 1em auto;
+}
+
+.mascotas{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1em;
+  padding: 3em;
+
+}
+</style>
---
+++ src/components/Tarjeta.vue
@@ -0,0 +1,77 @@
+<script setup>
+
+defineProps({
+  nombre:String,
+  tipo:String,
+  edad:Number,
+  vacunado:Boolean,
+  foto:String,
+  caracteristicas:Array
+})
+
+</script>
+
+<template>
+
+  <article>
+   <!--  ==== interpolaciones ==== -->
+    <h2>{{ nombre }}</h2>
+    <p>{{ tipo }}</p>
+    <p>{{ edad }} años</p>
+
+    <!-- ==== renderización condicional si está vacunado o no ==== -->
+    <p>
+      <span v-if="vacunado">Vacunado ✔</span>
+      <span v-else>No vacunado ❌</span>
+    </p>
+
+   <!--  ==== atributos dinámicos ==== -->
+    <img :src="foto" :alt="nombre">
+
+    <!-- ==== renderizado de listas ==== -->
+    <p class="lista">Características</p>
+    <ul v-for="(caracteristica, index) in caracteristicas" :key="index">
+      <li>{{caracteristica}}</li>
+    </ul>
+  </article>
+ 
+</template>
+
+<style>
+
+article{
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  padding: 1em;
+  border: solid 2px #424242;
+  border-radius: 5px;
+  flex-basis: 200px;
+}
+
+h2{
+  align-self: center;
+}
+img{
+  order: -1;
+  width: 50%;
+  aspect-ratio: 1/1;
+  object-fit: cover;
+  border-radius: 50%;
+  align-self: center;
+}
+.lista{
+  text-decoration: underline;
+  margin-block: .5em;
+}
+ul{
+  list-style-type: none;
+  padding-inline-start: 1em;
+}
+ul>li::marker{
+  content: "🐾 ";
+  
+}
+
+
+</style>
